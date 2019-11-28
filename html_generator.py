da87bcec0935aa3120dfd456ad32c97f8d1d7114
Ute Trautwein-Bruns <trautwein-bruns.ub.rwth-aachen.de>
2019-11-28 14:43:08 +0100

    added other files

---
+++ html_generator.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 13:58:12 2019
+
+@author: trautwein-bruns
+"""
+
+authors = ["Darwin", "Lovelace", "Hawkin", "Noether"]
+
+output_fh = open("list.html", "w")
+
+scaffold_start = """<!DOCTYPE html>
+<html>
+<head>
+<title>Titel dieser wunderbaren Seite</title>
+</head>
+<body>
+
+<h1>Eine grandiose Überschrift</h1>
+
+<p>Ein nicht weniger toller Abschnitt.</p>
+
+<ul>
+"""
+output_fh.write(scaffold_start)
+
+#<li>Ersten Ellement</li>
+#<li>Zweites Ellement</li>
+
+scaffold_middle = ""
+
+for author in authors:
+    scaffold_middle = scaffold_middle + "<li>" + author + "</li>\n"
+    
+output_fh.write(scaffold_middle)
+
+scaffold_end = """
+</ul>
+
+</body>
+
+</html>"""
+
+output_fh.write(scaffold_end)
+output_fh.close()
